fea4018268234292086cb7f2bcba9b62c0b1fa4f
proman3419 <proman7268@gmail.com>
2021-10-18 18:31:45 +0200

    Simplified code

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -5,31 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class World {
-    // do 14. punktu
-    public static void run1(String[] directions) {
-        System.out.println("Zwierzak idzie do przodu");
-        for (int i = 0; i < directions.length; i++) {
-            if (i < directions.length - 1)
-                System.out.printf("%s, ", directions[i]);
-            else
-                System.out.println(directions[i]);
-        }
-    }
-
-    // do 16. punktu
-    public static void run2(String[] directions) {
-        for (String direction : directions) {
-            switch (direction) {
-                case "f" -> System.out.println("Zwierzak idzie do przodu");
-                case "b" -> System.out.println("Zwierzak idzie do tyłu");
-                case "r" -> System.out.println("Zwierzak skręca w prawo");
-                case "l" -> System.out.println("Zwierzak skręca w lewo");
-            }
-        }
-    }
-
-    // ==============================================================================================================
-
     public static Direction convertStringToDirection(String string) {
         switch (string) {
             case "f" -> { return Direction.FORWARD; }
@@ -38,18 +13,6 @@ public class World {
             case "l" -> { return Direction.LEFT; }
             default -> { return Direction.NONE; }
         }
-    }
-
-    public static ArrayList<Direction> convertStringToDirection(String[] strings) {
-        ArrayList<Direction> directions = new ArrayList<>();
-
-        for (String string : strings) {
-            Direction direction = convertStringToDirection(string);
-            if (direction != Direction.NONE)
-                directions.add(direction);
-        }
-
-        return directions;
     }
 
     public static void run(Direction direction) {
@@ -61,14 +24,7 @@ public class World {
         }
     }
 
-    public static void run(ArrayList<Direction> directions) {
-        for (Direction direction : directions) {
-            run(direction);
-        }
-    }
-
-    // rozwiązanie w oparciu o stream
-    public static void streamSolution(String[] strings) {
+    public static void run(String[] strings) {
         List<String> stringsList = Arrays.asList(strings);
 
         stringsList
@@ -80,14 +36,7 @@ public class World {
     public static void main(String[] args) {
         System.out.println("Start");
 
-//        run1(args);
-
-//        run2(args);
-
-//        ArrayList<Direction> directions = convertStringToDirection(args);
-//        run(directions);
-
-        streamSolution(args);
+        run(args);
 
         System.out.println("Stop");
     }
